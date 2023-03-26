6feb84f0a689afd6593048c5022add1cef461425
Rajkumar19029 <87936393+Rajkumar19029@users.noreply.github.com>
2023-03-26 13:00:46 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,42 +1,62 @@
 const http = require("http");
 const fs = require("fs");
-const server = http.createServer(function (request, response) {
-  if (request.url === "/") {
-    fs.readFile("home.html", function (err, data) {
-      if (err) {
-        response.writeHead(500);
-        response.end("Error loading index.html");
-      } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(data);
-      }
-    });
-  } else if (request.url === "/project") {
-    fs.readFile("project.html", function (err, data) {
-      if (err) {
-        response.writeHead(500);
-        response.end("Error loading about.html");
-      } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(data);
-      }
-    });
-  } else if (request.url === "/registration") {
-    fs.readFile("registration.html", function (err, data) {
-      if (err) {
-        response.writeHead(500);
-        response.end("Error loading contact.html");
-      } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(data);
-      }
-    });
-  } else {
-    // handle 404 error
-    response.writeHead(404);
-    response.end("Page not found");
+const path = require("path");
+const url = require("url");
+const { argv } = require("process");
+
+const port = argv.includes("--port") ? argv[argv.indexOf("--port") + 1] : 8080;
+
+const server = http.createServer((req, res) => {
+  let filePath = "." + req.url;
+  if (filePath === "./") {
+    filePath = "./home.html";
+  } else if (filePath === "./project") {
+    filePath = "./project.html";
+  } else if (filePath === "./registration") {
+    filePath = "./registration.html";
   }
+
+  const extname = path.extname(filePath);
+  let contentType = "text/html";
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        fs.readFile("./404.html", (err, content) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(content, "utf-8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end(
+          "Sorry, check with the site admin for error: " + err.code + " ..\n"
+        );
+        res.end();
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
 });
-server.listen(3000, function () {
-  console.log("Server is listening on port 3000");
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
